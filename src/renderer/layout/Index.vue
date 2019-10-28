7a36f12d1eb0173d34ed8e272f7fe4a8e9ee4671
yojhop <yojhop@126.com>
2019-10-28 16:14:40 +0800

    #1176 完成点击执行命令功能(windows)

--- src/renderer/layout/Index.vue
+++ src/renderer/layout/Index.vue
@@ -11,25 +11,68 @@
     <div class="app-main">
       <router-view/>
     </div>
-    <foot />
+    <div>
+      <button style='margin-left:5px;padding:3px' v-for="(tool,idx) in tools" @click='runCmd(tool.cmd)' :key='idx'>{{tool.key}}</button>
+    </div>
+    <div>outputs</div>
+    <div v-for='(output,idx) in outputs' :key='idx'>
+      {{output}}
+    </div>
+    <!-- <foot /> -->
   </div>
 </template>
 
 <script>
 import Foot from './components/Foot'
 import Navbar from './components/Navbar'
+import * as yaml from 'js-yaml'
+import * as path from 'path'
+import * as fs from 'fs'
+import {exec} from 'shelljs'
 // import ResizeMixin from './mixin/resizeHandler'
-
+import cmd from 'node-cmd'
 export default {
   name: 'APP',
   components: {
     Foot,
     Navbar
   },
+  data(){
+    return {
+      outputs:[],
+      tools: []
+    }
+  },
   // mixins: [ResizeMixin],
   computed: {
   },
   methods: {
+    runEcho(){
+      cmd.get(
+        'echo hello',
+        function(err, data, stderr){
+            console.log(err,data);
+        }
+    );
+    },
+    runCmd(cmdLine){
+     exec(this.getcmdLine(cmdLine), (code, stdout, stderr)=> {
+      console.log('Exit code:', code);
+      console.log('Program output:', stdout);
+      console.log('Program stderr:', stderr);
+    })
+    },
+    getcmdLine(cmd){
+      if(process.platform==='win32') return `start cmd.exe /K ${cmd}`
+      return cmd
+    }
+  },
+  created(){
+    let filePath = process.env.NODE_ENV === 'production' ? path.join(__dirname, '../../src/renderer/assets/configs/tools.yml') : path.join(__dirname, '../assets/configs/tools.yml')
+    let tools = yaml.safeLoad(fs.readFileSync(filePath, 'utf8'))
+    for(let key in tools){
+      this.tools.push({key,cmd:tools[key]})
+    }
   }
 }
 </script>
